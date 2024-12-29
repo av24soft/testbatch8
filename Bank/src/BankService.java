995bbf31a5f75d59fe3d3c09a432bf643c4925b6
av24soft <av24soft@gmail.com>
2024-12-29 09:20:58 +0530

    story123

---
+++ Bank/src/BankService.java
@@ -0,0 +1,25 @@
+
+public class BankService {
+
+	
+	void deposite(String name, int acNo, int amount) {
+
+		System.out.println(" hi deposited");
+
+	}
+
+	int withdrwal(String name, int acNo, int amount, int pin) {
+
+		System.out.println("withdrwal done");
+
+		return 10000;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
